c3fdfbee08eb7bd22701f810b89584e80576d1c8
jaonog <jaonogueira77@gmail.com>
2025-10-08 22:29:33 -0300

    Primeira versão

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/home.module.css"
+
+export default function Home () {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas + Organização</title>
+      </Head>
+      <h1>Tarefa Plus</h1>
+    </div>
+    
+  )
+}
+
